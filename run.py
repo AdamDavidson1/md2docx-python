fe181e95271927ca125bb8a58d8853691182cd73
shloktech <shloktadilkar@gmail.com>
2025-01-01 23:06:41 +0530

    package refactoring and import rename

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from md2docx_python import markdown_to_word
+from md2docx_python.src.md2docx_python import markdown_to_word
 
-markdown_file = "md2docx_python-python\sample_files\amazon_case_study.docx"
-word_file = "md2docx_python-python\sample_files\amazon_case_study2.md"
+markdown_file = "sample_files/amazon_case_study.md"
+word_file = "sample_files/amazon_case_study.docx"
 
 markdown_to_word(markdown_file, word_file)
